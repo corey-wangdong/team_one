7aed5fe89b44f116bbd84a7115d3ac4806cd8cf0
corey <15379675451@163.com>
2021-01-10 13:56:17 +0800

    update banner data

--- index/utils/getBoxDataList.js
+++ index/utils/getBoxDataList.js
@@ -19,11 +19,43 @@ function getBoxDataList (boxDom,dataList) {
     }
 	tempDom.html(tempStr);
 }
+function getJtxwBannerTitle(boxDom,dataList) {
+	var tempDom= $(boxDom);
+	if (!tempDom) return;
+
+	let tempStr = `<div class="tltBox" style="width:1130px; position:relative;">
+	<div class="tlt_1"><a href="javascript:openMenu(${dataList.titleContent.linkUrl})"><font color="#c10000">${dataList.titleContent.preTitle}</font><font color="#000">${ dataList.titleContent.lastTitle}</font></a></div>     	
+	<div class="more"><a href="javascript:openMenu(${dataList.titleContent.linkUrl})">更多 &gt;</a></div>
+	</div>`;
+
+	tempDom.html(tempStr);
+}
+
+function getJtxwBannerList(boxDom,dataList) {
+	var tempDom= $(boxDom);
+	if (!tempDom) return;
+
+	let bannerImgUrlData = dataList.bannerImgUrlData || [];
+	let listStr = '';
+	for(var i=0; i < bannerImgUrlData.length; i++) {
+		listStr += `<script type="text/javascript">rotaionArr[rotaionArr.length]= '${bannerImgUrlData[i].linkUrl}'</script>
+		<li><a href="javascript:openWin('${bannerImgUrlData[i].linkUrl}')"><img src="${bannerImgUrlData[i].imgUrl}" alt='${bannerImgUrlData[i].alt}'></a></li>`
+	}
+	console.log('listStr----',listStr)
+	let tempStr = `<script type="text/javascript">var rotaionArr= new Array();</script>
+	<div id="yx-rotaion" class="yx-rotaion" style="height:435px;">
+	<ul class="rotaion_list">${listStr}</ul>
+	</div>
+	<script type="text/javascript" src="./index/js/jquery.yx_rotaion.js"></script>
+	<script type="text/javascript">$(".yx-rotaion").yx_rotaion({auto:true});</script>`;
+
+	tempDom.html(tempStr);
+}
 
 function getJtxwDataList (boxDom,dataList) {
 	var tempDom= $(boxDom);
 	if (!tempDom) return;
-	listStr = `<h1><a href="./index/components/articles.html?${dataList.jtNewsUrl}">${dataList.jtNewsTitle}</a></h1>
+	let listStr = `<h1><a href="./index/components/articles.html?${dataList.jtNewsUrl}">${dataList.jtNewsTitle}</a></h1>
 	<h2>${dataList.jtNewsContent}<a href="./index/components/articles.html?${dataList.jtNewsUrl}" class="detailLink">[详情]</a>
 				</h2><ul style="margin-top: 10px;">`;
 	var tempData = dataList.contentData;
@@ -44,7 +76,7 @@ function getJtxwDataList (boxDom,dataList) {
 function getDataList (boxDom,dataList,content) {
 	var tempDom= $(boxDom);
 	if (!tempDom || dataList.length === 0) return;
-	listStr = '';
+	let listStr = '';
 	for(var i=0; i < dataList.length; i++) {
 		listStr += `<li>${content ? content : ''}<a data-url='${dataList[i].url}' href="#">${dataList[i].title}</a></li>`
 	}
@@ -56,3 +88,4 @@ function getDataList (boxDom,dataList,content) {
 			}
 	})
 }
+
